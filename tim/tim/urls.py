2ae7e37bc6ea94ed712398a12fc02e2d0e25b426
Tim Saunders <tim@sauntimo.org>
2013-04-20 22:25:08 +0000

    Redirect / --> /blog

--- tim/tim/urls.py
+++ tim/tim/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,4 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
      url(r'^blog/', include(blog.urls)),
+     url(r'^/?$', RedirectView.as_view(url='/blog')),
 )
